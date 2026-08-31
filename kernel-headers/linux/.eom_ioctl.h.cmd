8ce4ba8911872ce19ad0b002692a0bbb08fed35a
Sean Du <do4suki@gmail.com>
2026-08-31 22:53:19 +0800

    import
    Signed-off-by: Sean Du <do4suki@gmail.com>

---
+++ kernel-headers/linux/.eom_ioctl.h.cmd
@@ -0,0 +1,1 @@
+cmd_usr/include/linux/eom_ioctl.h := sh ../scripts/headers_install.sh ../include/uapi/linux/eom_ioctl.h usr/include/linux/eom_ioctl.h
---
+++ kernel-headers/linux/.qti_virtio_mem.h.cmd
@@ -0,0 +1,1 @@
+cmd_usr/include/linux/qti_virtio_mem.h := sh ../scripts/headers_install.sh ../include/uapi/linux/qti_virtio_mem.h usr/include/linux/qti_virtio_mem.h
